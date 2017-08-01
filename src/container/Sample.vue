355178a442b4f0abb2765bed819e0b51d5143eca
Sagar Makhija <sagar1309@live.com>
2017-08-02 03:22:28 +0530

    sample ready

--- src/container/Sample.vue
+++ src/container/Sample.vue
@@ -4,64 +4,17 @@
 
       <div class="template-body-main">
         <!-- seller -->
-        <div class="flex-head-items">
-          <div class="seller">
-            <h1 class="title is-2 has-text-centered">Siddhartha Wear</h1>
-            <div class="tile is-ancestor">
-              <div class="tile is-parent">
-                <article class="tile is-child">
-                  <div class="table-responsive">
-                    <table class="table is-bordered is-striped is-narrow">
-                      <tbody>
-                        <tr> <th>GST NO:</th> <td>24ACDPM5595M1Z3</td> </tr>
-                        <tr> <th>Address:</th> <td>2858, 2nd Floor, Opp. Anand Sweets,</td> </tr>
-                        <tr> <td colspan="2">Kalupur Kotni Rang, Ahmedabad, Gujarat, 380001</td> </tr>
-                        <tr> <th>Phone:</th> <td>079-22168689</td> </tr>
-                        <tr> <th>Email:</th> <td>siddharthawear2858@gmail.com</td> </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </article>
-              </div>
-            </div>
-          </div>
-          <div class="invoice">
-            <p class="invoice-details-copy"><b>Customer Copy</b></p>
-            <span class="invoice-head">INVOICE</span>
-            <p class="invoice-details"><b>Invoice No: 12345</b></p>
-            <p class="invoice-details"><b>Date: 13/09/1993</b></p>
-            <p class="invoice-details"><b>State Code: 24</b></p>
-          </div>
-        </div>
-      </div>
-      <!-- seller -->
-      <!-- buyer -->
-      <div class="buyer">
-        <div class="part-table">
-
+        <div class="seller">
+          <h1 class="title is-2 has-text-centered">Siddhartha Wear</h1>
           <div class="tile is-ancestor">
             <div class="tile is-parent">
               <article class="tile is-child">
                 <div class="table-responsive">
                   <table class="table is-bordered is-striped is-narrow">
                     <tbody>
-                      <tr> <th>M/S:</th> <td>Shanti Textile</td> </tr>
-                      <tr><th>GST NO:</th> <td>24ACDPM5595M1Z3</td> </tr>
+                      <tr> <th>GST NO:</th> <td>24ACDPM5595M1Z3</td> </tr>
                       <tr> <th>Address:</th> <td>2858, 2nd Floor, Opp. Anand Sweets,</td> </tr>
                       <tr> <td colspan="2">Kalupur Kotni Rang, Ahmedabad, Gujarat, 380001</td> </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-ancestor">
-            <div class="tile is-parent">
-              <article class="tile is-child">
-                <div class="table-responsive right-side">
-                  <table class="table is-bordered is-striped is-narrow">
-                    <tbody>
-                      <!-- <tr> <th>Agent:</th> <td>Dummy Name</td> </tr> -->
                       <tr> <th>Phone:</th> <td>079-22168689</td> </tr>
                       <tr> <th>Email:</th> <td>siddharthawear2858@gmail.com</td> </tr>
                     </tbody>
@@ -69,6 +22,49 @@
                 </div>
               </article>
             </div>
+          </div>
+        </div>
+        <div class="invoice">
+          <p class="invoice-details-copy"><b>Customer Copy</b></p>
+          <span class="invoice-head">INVOICE</span>
+          <p class="invoice-details"><b>Invoice No: 12345</b></p>
+          <p class="invoice-details"><b>Date: 13/09/1993</b></p>
+          <p class="invoice-details"><b>State Code: 24</b></p>
+        </div>
+      </div>
+      <!-- seller -->
+      <!-- buyer -->
+      <div class="buyer">
+
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child">
+              <div class="table-responsive">
+                <table class="table is-bordered is-striped is-narrow">
+                  <tbody>
+                    <tr> <th>M/S:</th> <td>Shanti Textile</td> </tr>
+                    <tr><th>GST NO:</th> <td>24ACDPM5595M1Z3</td> </tr>
+                    <tr> <th>Address:</th> <td>2858, 2nd Floor, Opp. Anand Sweets,</td> </tr>
+                    <tr> <td colspan="2">Kalupur Kotni Rang, Ahmedabad, Gujarat, 380001</td> </tr>
+                  </tbody>
+                </table>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <article class="tile is-child">
+              <div class="table-responsive right-side">
+                <table class="table is-bordered is-striped is-narrow">
+                  <tbody>
+                    <!-- <tr> <th>Agent:</th> <td>Dummy Name</td> </tr> -->
+                    <tr> <th>Phone:</th> <td>079-22168689</td> </tr>
+                    <tr> <th>Email:</th> <td>siddharthawear2858@gmail.com</td> </tr>
+                  </tbody>
+                </table>
+              </div>
+            </article>
           </div>
         </div>
       </div>
@@ -159,6 +155,94 @@
                       <td>250</td>
                       <td>4750</td>
                     </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
+                    <tr>
+                      <td>1</td>
+                      <td>HoneyComb T-Shirt - 12345 </td>
+                      <td>L-XXL</td>
+                      <td>50</td>
+                      <td>100</td>
+                      <td>5000</td>
+                      <td>5</td>
+                      <td>250</td>
+                      <td>4750</td>
+                    </tr>
 
                   </tbody>
                 </table>
@@ -167,119 +251,121 @@
           </div>
         </div>
         <div class="bottom-details">
-          <!-- <div class="part-one has-text-centered">
-          <strong>Amount: forty-two thousand, six hundred fifty-four</strong>
-        </div> -->
-        <div class="tile is-ancestor part-two">
-          <div class="tile is-parent">
-            <article class="tile is-child">
-              <div class="table-responsive">
-                <table class="table is-bordered is-striped is-narrow">
-                  <tbody>
-                    <tr> <td>Total Taxable Amount: </td> <th>&#8377; 4750</th> </tr>
-                    <tr> <td>CGST: </td> <th>&#8377; 118.75</th> </tr>
-                    <tr> <td>SGST: </td> <th>&#8377; 118.75</th> </tr>
-                    <tr> <td>IGST: </td> <th>&#8377; 0</th> </tr>
-                    <tr> <td>Total Invoice Value: </td> <th>&#8377; 4980</th> </tr>
-                  </tbody>
-                </table>
-              </div>
-            </article>
+          <div class="part-one has-text-centered">
+            <strong>Amount: forty-two thousand, six hundred fifty-four</strong>
+          </div>
+          <div class="tile is-ancestor part-two">
+            <div class="tile is-parent">
+              <article class="tile is-child">
+                <div class="table-responsive">
+                  <table class="table is-bordered is-striped is-narrow">
+                    <tbody>
+                      <tr> <td>Total Taxable Amount: </td> <th>&#8377; 4750</th> </tr>
+                      <tr> <td>CGST: </td> <th>&#8377; 118.75</th> </tr>
+                      <tr> <td>SGST: </td> <th>&#8377; 118.75</th> </tr>
+                      <tr> <td>IGST: </td> <th>&#8377; 0</th> </tr>
+                      <tr> <td>Total Invoice Value: </td> <th>&#8377; 4980</th> </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <!-- table -->
+      <!-- table -->
 
-    <!-- bottom -->
-    <div class="template-bottom footer">
-      <div class="part-one">
-        <span><b>N.B.</b></span>
-        <p><b>(1)</b> Interest will be charged @ 18% from the date of bill <b>(2)</b> The Payment
-          of this bill should be made by demand draft of our name duly crossed  by
-          payee's A/c. only <b>(3)</b> Goods are sent on your account & risk and according
-          to the rules of the Gujarat Garment Manufacturer Association(GGMA).</p>
-          <p class="spacing">Subject to Ahmedabad Jurisdiction</p>
-        </div>
-        <div class="part-two">
-          <p>For, Siddhartha Wear</p>
-          <p class="take-me-to-the-bottom">Authorised Signature</p>
-        </div>
-      </div>
       <!-- bottom -->
+      <div class="template-bottom">
+        <div class="part-one">
+          <span><b>N.B.</b></span>
+          <p><b>(1)</b> Interest will be charged @ 18% from the date of bill <b>(2)</b> The Payment
+            of this bill should be made by demand draft of our name duly crossed  by
+            payee's A/c. only <b>(3)</b> Goods are sent on your account & risk and according
+            to the rules of the Gujarat Garment Manufacturer Association(GGMA).</p>
+            <p class="spacing">Subject to Ahmedabad Jurisdiction</p>
+          </div>
+          <div class="part-two">
+            <p>For, Siddhartha Wear</p>
+            <p class="take-me-to-the-bottom">Authorised Signature</p>
+          </div>
+        </div>
+        <!-- bottom -->
 
+      </div>
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-export default {
-  name: 'sample',
-}
-</script>
-
-<style lang="scss">
-.sample {
-  // font-size: 10px;
-  font: -webkit-small-control;
-  .box {
-    padding: 0;
+  <script>
+  export default {
+    name: 'sample',
   }
+  </script>
 
-  .template-body-main {
-    padding: 2rem;
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    .flex-head-items {
+  <style lang="scss">
+  .sample {
+    // width: 21cm;
+    // height: 29.7cm;
+    // background: burlywood;
+    // margin: 0 auto;
+    // margin-top: 0.2cm;
+    // margin-bottom: 0.5cm;
+    font: -webkit-small-control;
+    .box {
+      padding: 0;
+    }
+
+    .template-body-main {
+      padding: 1.5rem;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
       display: flex;
       justify-content: space-between;
       border-bottom: solid 2px #ddd;
       .tile.is-ancestor {
         .table-responsive {
           .table.is-bordered.is-striped.is-narrow {
-            margin-bottom: 1rem;
+            margin-bottom: 0;
           }
         }
       }
     }
-  }
 
-  .seller {
-    .title.is-2 {
-      font-weight: bold;
-      margin: 0;
-      padding-bottom: 0.5rem;
-      background: whitesmoke;
+    .seller {
+      .title.is-2 {
+        font-weight: bold;
+        margin: 0;
+        padding-bottom: 0.5rem;
+        background: whitesmoke;
+      }
     }
-  }
 
-  .invoice {
-    p {
-      text-align: center;
+    .invoice {
+      p {
+        text-align: center;
+      }
+      .invoice-head {
+        color: grey;
+        font-weight: 500;
+        font-size: -webkit-xxx-large;
+      }
+      .invoice-details {
+        font-size: 0.8rem;
+      }
+      .invoice-details-copy {
+        font-size: 0.8rem;
+      }
     }
-    .invoice-head {
-      color: grey;
-      font-weight: 500;
-      font-size: -webkit-xxx-large;
-    }
-    .invoice-details {
-      font-size: 0.8rem;
-    }
-    .invoice-details-copy {
-      font-size: 0.8rem;
-    }
-  }
 
-  .table-responsive {
-    // width: 28rem;
-  }
+    .table-responsive {
+      // width: 28rem;
+    }
 
 
-  .buyer {
-    padding: 2rem;
-    padding-top: 0;
-    padding-bottom: 0;
-    .part-table {
+    .buyer {
+      padding: 1.5rem;
+      padding-top: 1rem;
+      padding-bottom: 1rem;
       border-bottom: solid 2px #ddd;
       display: flex;
       justify-content: space-between;
@@ -289,67 +375,75 @@ export default {
       .tile.is-parent {
         padding-bottom: 0;
         .table.is-bordered.is-striped.is-narrow {
-          margin-bottom: 1rem;
+          margin-bottom: 0;
         }
       }
-    }
-    .right-side {
-      float: right;
-    }
-  }
-
-  .template-body-items {
-    padding: 2rem;
-    padding-top: 1rem;
-    border-bottom: solid 2px #ddd;
-    th, td{
-      text-align: center;
-    }
-    .tile.is-ancestor {
-      margin-bottom: 0;
-    }
-    .tile.is-parent {
-      padding-bottom: 0;
-      .table.is-bordered.is-striped.is-narrow {
-        margin: 0;
-      }
-    }
-
-    .bottom-details {
-      .part-one {
-
-        // margin: auto;
-      }
-      .table-responsive {
-        width: fit-content;
+      .right-side {
         float: right;
       }
     }
-  }
 
-  .template-bottom {
-    // position: relative;
-    // margin-top: 15rem;
-    // position: fixed;
-    padding: 2rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-    .part-one {
-      max-width: 23rem;
-      .spacing {
-        padding-top: 1.5rem;
+    .template-body-items {
+      height: 15cm;
+      padding: 2rem;
+      padding-top: 1rem;
+      border-bottom: solid 2px #ddd;
+      th, td{
+        text-align: center;
+      }
+      .tile.is-ancestor {
+        margin-bottom: 0;
+      }
+      .tile.is-parent {
+        padding-bottom: 0;
+        .table.is-bordered.is-striped.is-narrow {
+          margin: 0;
+        }
+      }
+
+      .bottom-details {
+        display: flex;
+        justify-content: space-between;
+        .part-one {
+          border: solid 1px #ddd;
+          height: fit-content;
+          display: flex;
+          align-items: flex-end;
+          margin-left: 6rem;
+          margin-top: auto;
+        }
+        .table-responsive {
+          width: fit-content;
+          float: right;
+        }
       }
     }
-    .part-two {
-      p {
-        font-weight: 700;
+
+    .template-bottom {
+      // position: relative;
+      // margin-top: 15rem;
+      // position: fixed;
+      // padding: 2rem;
+      display: flex;
+      justify-content: space-between;
+      .part-one {
+        max-width: 23rem;
+        .spacing {
+          padding-top: 1.5rem;
+        }
       }
-      .take-me-to-the-bottom {
-        bottom: 0;
-        margin-top: 65%;
+      .part-two {
+        display: flex;
+        justify-content: space-between;
+        flex-direction: column;
+        p {
+          font-weight: 700;
+        }
       }
     }
-  }
+    #header, #footer {
+   visibility: hidden !important;
+   display: none !important;
 }
-</style>
+  }
+  </style>
